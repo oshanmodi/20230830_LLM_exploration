4ae93b9cf5787edf3a00ae2487e8200b1a9c50ef
Oshan <modi.oshan@gmail.com>
2023-08-30 19:13:38 +0530

    add gitignore and pip files.

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+import config
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+headers = {"Authorization": f"Bearer {config.hf['api_key']}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": "Astronaut riding a horse",
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
